29a73259a52d0aafdd037b538261e85f38c336e1
simonkey1 <cristobalc.gomez@mail.udp.cl>
2025-02-12 01:38:18 -0300

    holaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,21 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://powerbi-project.netlify.app/blog/",
-
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+    },
+  },
   vite: {
     define: {
       "process.env.LANG": JSON.stringify("es_ES.UTF-8"),
     },
+
+    plugins: [tailwindcss()],
   },
 
   integrations: [preact()],
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}", // 📌 Asegura que Tailwind lee estos archivos
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
